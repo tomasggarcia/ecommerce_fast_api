d55697b17f4cafe595f4243598731bda40848245
Tomas Garcia <tgarcia@w3itsolutions.net>
2021-11-28 15:12:27 -0300

    model user added

---
+++ models.py
@@ -0,0 +1,16 @@
+from typing import List, Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+class Role(str, Enum):
+    admin = 'admin'
+    user = 'user'
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    email: str
+    role: List[Role]
+    first_name: str
+    last_name: str
+    
